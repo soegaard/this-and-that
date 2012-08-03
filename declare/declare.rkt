0ad140729f3a11d7b4d454bab7f8d6e55306d406
Jens Axel Søgaard <jensaxel@soegaard.net>
2012-08-03 19:50:44 +0200

    Fixed bug.

--- declare/declare.rkt
+++ declare/declare.rkt
@@ -35,15 +35,44 @@
                 (define mut-str  (format "~a" (syntax-e m)))
                 (define suffix (substring mut-str (+ 5 (string-length type-str))))
                 (format-id stx "~a.~a" #'id suffix))))
+       (define (mutator->++ m)
+         (and (symbol? (syntax-e m))
+              (let ()
+                (define type-str (format "~a" (syntax-e #'type)))
+                (define mut-str  (format "~a" (syntax-e m)))
+                (define suffix (substring mut-str (+ 5 (string-length type-str))))
+                (format-id stx "~a.~a++" #'id suffix))))
+       (define (mutator->+= m)
+         (and (symbol? (syntax-e m))
+              (let ()
+                (define type-str (format "~a" (syntax-e #'type)))
+                (define mut-str  (format "~a" (syntax-e m)))
+                (define suffix (substring mut-str (+ 5 (string-length type-str))))
+                (format-id stx "~a.~a+=" #'id suffix))))
+       (define (mutator->acc m)
+         (and (symbol? (syntax-e m))
+              (let ()
+                (define type-str (format "~a" (syntax-e #'type)))
+                (define mut-str  (format "~a" (syntax-e m)))
+                (define suffix! (substring mut-str (+ 5 (string-length type-str))))
+                (define suffix  (substring suffix! 0 (sub1 (string-length suffix!))))
+                (format-id stx "~a-~a" #'type suffix))))
        (with-syntax ([(info constructor predicate? accessors mutators super)
                       (extract-struct-info (syntax-local-value #'type))])
          (with-syntax ([(s.accessor ...) (syntax-map accessor->dot #'accessors)]
                        [(accessor ...)   #'accessors]
                        [(s.mutator ...)  (filter values (syntax-map mutator->dot #'mutators))]
-                       [(mutator ...)    (filter values (syntax->list #'mutators))])
+                       [(mutator ...)    (filter values (syntax->list #'mutators))]
+                       [(s.++ ...)  (filter values (syntax-map mutator->++ #'mutators))]
+                       [(++ ...)    (filter values (syntax->list #'mutators))]
+                       [(s.+= ...)  (filter values (syntax-map mutator->+= #'mutators))]
+                       [(+= ...)    (filter values (syntax->list #'mutators))]
+                       [(mut-acc ...)  (filter values (syntax-map mutator->acc #'mutators))])
            #'(let-syntax ([s.accessor (λ (so) #'(accessor id))] ...)
                (let-syntax ([s.mutator (λ (so) (syntax-case so () [(_ val) #'(mutator id val)]))] ...)
-                 (declare (decl ...) . body))))))]
+                 (let-syntax ([s.++ (λ (so) (syntax-case so () [(_) #'(mutator id (add1 (mut-acc id)))]))] ...)
+                   (let-syntax ([s.+= (λ (so) (syntax-case so () [(_ val) #'(mutator id (+ (mut-acc id) val))]))] ...)
+                     (declare (decl ...) . body))))))))]
     [_ (error)]))
 
 
@@ -59,6 +88,11 @@
   (define (skip-one s)
     (declare ([s sub])
       (s.start! (+ s.start 1))))
+  (define (skip-two s)
+    (declare ([s sub])
+      (s.start!+= 2)))  
   (check-equal? (sub->string sub1) "This")
   (skip-one sub1)
-  (check-equal? (sub->string sub1) "his"))
+  (check-equal? (sub->string sub1) "his")
+  (skip-two sub1)
+  (check-equal? (sub->string sub1) "s"))
